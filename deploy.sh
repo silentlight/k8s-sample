dd36fbf28bb1e76a71587427e348b8562b5bfccb
Bartosz Hejman <bartoszhejman@gmail.com>
2019-08-25 00:36:36 +0200

    Fix server build during deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
+# Build Docker images
 docker build -t synergee/multi-client:latest -t synergee/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t synergee/multi-server:latest -t synergee/multi-server:$SHA -f ./sever/Dockerfile ./server
+docker build -t synergee/multi-server:latest -t synergee/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t synergee/multi-worker:latest -t synergee/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
+# Push Docker images to registry
 docker push synergee/multi-client:latest
 docker push synergee/multi-client:$SHA
 
@@ -11,8 +13,10 @@ docker push synergee/multi-server:$SHA
 docker push synergee/multi-worker:latest
 docker push synergee/multi-worker:$SHA
 
+# Apply Kubernetes cluster
 kubectl apply -f k8s
 
+# Set image implicitly to latest version
 kubectl set image deployments/client-deployment client=synergee/multi-client:$SHA
 kubectl set image deployments/server-deployment server=synergee/multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=synergee/multi-worker:$SHA
